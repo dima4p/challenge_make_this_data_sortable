fd8bee6523b762e80e6193698224b08f6202f104
Dmitri Koulikoff <dima@koulikoff.ru>
2015-06-24 01:39:43 +0300

    Scopes ProgrammingLanguage.with and .without are added !

--- app/models/programming_language.rb
+++ app/models/programming_language.rb
@@ -18,4 +18,42 @@ class ProgrammingLanguage < ActiveRecord::Base
 
   scope :ordered, -> { order(:name) }
 
+  # Selects the records containing given words case insensitive
+  scope :with, -> words {
+    return ordered if words.blank?
+    words = Array.wrap(words).map(&:downcase)
+    logger.debug "ProgrammingLanguage@#{__LINE__}.with #{words.inspect}" if logger.debug?
+    cond = words.map do |word|
+      "(#{[
+        "lower(name) LIKE '%#{word}%'",
+        "(SELECT count(*) FROM unnest(language_type) AS a(t) WHERE lower(t) LIKE '%#{word}%') > 0",
+        "(SELECT count(*) FROM unnest(designed_by) AS a(t) WHERE lower(t) LIKE '%#{word}%') > 0"
+      ].join ' OR '})"
+    end.join ' AND '
+    count = words.map do |word|
+      [
+        "CASE WHEN lower(name) LIKE '%#{word}%' THEN 1 ELSE 0 END",
+        "(SELECT count(*) FROM unnest(language_type) AS a(t) WHERE lower(t) LIKE '%#{word}%')",
+        "(SELECT count(*) FROM unnest(designed_by) AS a(t) WHERE lower(t) LIKE '%#{word}%')"
+      ]
+    end.flatten.join(' + ')
+    select("* , #{count} AS rate")
+      .where(cond)
+      .order('rate DESC')
+      .order('name')
+  }
+
+  # Rejects the records containing given words case insensitive
+  scope :without, -> words {
+    logger.debug "ProgrammingLanguage@#{__LINE__}.without #{words.inspect}" if logger.debug?
+    cond = Array.wrap(words).map(&:downcase).map do |word|
+      [
+        "lower(name) NOT LIKE '%#{word}%'",
+        "(SELECT count(*) FROM unnest(language_type) AS a(t) WHERE lower(t) LIKE '%#{word}%') = 0",
+        "(SELECT count(*) FROM unnest(designed_by) AS a(t) WHERE lower(t) LIKE '%#{word}%') = 0"
+      ]
+    end.flatten.join ' AND '
+    where cond
+  }
+
 end
--- spec/models/programming_language_spec.rb
+++ spec/models/programming_language_spec.rb
@@ -20,11 +20,71 @@ describe ProgrammingLanguage, type: :model do
   it {should validate_presence_of :name}
 
   describe :class do
-    it 'should respond to :ordered' do
-      create :programming_language
-      create :programming_language
-      expect(ProgrammingLanguage.ordered).to eq ProgrammingLanguage.order(:name)
-    end
-  end
+    describe :scopes do
+      let(:ada) do
+        create :programming_language, name: 'Ada',
+                                      language_type: %w[Compiled Imperative],
+                                      designed_by: ["Tucker Taft", "Jean Ichbiah"]
+      end
+
+      let(:ant) do
+        create :programming_language, name: 'Ant',
+                                      language_type: %w[Interpreted],
+                                      designed_by: ["Jean Software Foundation"]
+      end
+
+      before :each do
+        ant
+        ada
+      end
+
+      describe :ordered do
+        it 'orders the records by :name' do
+          expect(ProgrammingLanguage.ordered).to eq ProgrammingLanguage.order(:name)
+        end
+      end   # :ordered
+
+      describe :with do
+        it 'filters out the records that do not contain any of the given words case insensitive' do
+          expect(ProgrammingLanguage.with(['ada']).map(&:id))
+            .to eq [ada.id]
+          expect(ProgrammingLanguage.with(['compiled']).map(&:id))
+            .to eq [ada.id]
+          expect(ProgrammingLanguage.with(['tucker']).map(&:id))
+            .to eq [ada.id]
+          expect(ProgrammingLanguage.with(['O']).map(&:id))
+            .to eq [ada.id, ant.id]
+          expect(ProgrammingLanguage.with(%w[Tucker Interpreted]).map(&:id))
+            .to eq []
+        end
+
+        it 'orders the languages with the same rate by name' do
+          expect(ProgrammingLanguage.with(['Jean']).map(&:rate))
+            .to eq [1, 1]
+          expect(ProgrammingLanguage.with(['Jean']).map(&:id))
+            .to eq [ada.id, ant.id]
+        end
+
+        it 'returns the ordered collection if words are empty' do
+          expect(ProgrammingLanguage.with([]))
+            .to eq ProgrammingLanguage.order(:name)
+        end
+      end   # :with
+
+      describe :without do
+        it 'filters out the records that contain any of the given words case insensitive' do
+          expect(ProgrammingLanguage.without(['ada']).map(&:id)).to eq [ant.id]
+          expect(ProgrammingLanguage.without(['compiled']).map(&:id)).to eq [ant.id]
+          expect(ProgrammingLanguage.without(['tucker']).map(&:id)).to eq [ant.id]
+          expect(ProgrammingLanguage.without(['O']).map(&:id)).to eq []
+        end
+
+        it 'returns the original collection if words are empty' do
+          expect(ProgrammingLanguage.ordered.without([]))
+            .to eq ProgrammingLanguage.order(:name)
+        end
+      end   # :without
+    end   # :scopes
+  end   # :class
 
 end
